d563bf1de98baffe3787b9f0d300a5d013ec8350
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-05-26 20:02:55 -0400

    Add files via upload

---
+++ node_helper.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser');
+const NodeHelper = require('node_helper'); // eslint-disable-line import/no-unresolved
+
+module.exports = NodeHelper.create({
+  start() {
+    this._initHandler();
+  },
+
+  socketNotificationReceived(notificationName, payload) {
+    if (notificationName === 'MMM-RemoteTemperature.INIT') {
+      console.log(`MMM-RemoteTemperature Node helper: Init notification received from module for sensor "${payload.sensorId}".`); // eslint-disable-line no-console
+    }
+  },
+
+  _initHandler() {
+    this.expressApp.use(bodyParser.json());
+    this.expressApp.post('/remote-temperature', this._onTemperatureValueReceived.bind(this));
+  },
+
+  _onTemperatureValueReceived(req, res) {
+    const params = req.body;
+
+    const payload = {
+      temp: params.temp,
+      humidity: params.humidity,
+      battery: params.battery,
+      sensorId: params.sensorId
+    };
+
+    this.sendSocketNotification('MMM-RemoteTemperature.VALUE_RECEIVED', payload);
+
+    res.sendStatus(200);
+  }
+});
